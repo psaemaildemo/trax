ad89feb20d6bb83b96a3e2c8cd0457fdd8a73370
psademoemail <psaemaildemouser@gmail.com>
2022-06-16 19:22:39 +0530

    Developed following modules 1. Lead, 2. Contacts, 3. Billing, 4. Email

---
+++ src/main/java/com/trax/repositories/ContactRepository.java
@@ -0,0 +1,10 @@
+package com.trax.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trax.entities.Contact;
+
+public interface ContactRepository extends JpaRepository<Contact, Long>
+{
+
+}
